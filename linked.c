73715ed6d61e0aff7de4f5669fdd935772ca1231
xoaeb100 <30340405+xoaeb100@users.noreply.github.com>
2017-09-08 15:51:23 +0530

    Add files via upload

---
+++ linked.c
@@ -0,0 +1,83 @@
+/
+ * BY
+
+
+
+
+
+
+
+
+
+#include<stdio.h>
+#include<stdio.h>
+
+typedef struct linked_list{
+	int data;
+	struct linked_list * next;
+}node;
+void print(node *q)
+{
+	}
+{
+	node *temp *ptr
+	temp=*q;
+	ptr=(node*)malloc(sizeof(node));
+	ptr->data=no;
+	if (temp==NULL)
+	 ptr->next=NULL;
+	 else
+	 while(temp->next=temp;
+	 *q=ptr
+	 
+	printf("Enter location whrer data is to be inserted:");
+	scanf("%d"&loc);
+	for(k=1)}
+void del(node**q,int no)
+
+int main()
+{
+	node *star, *ptr, Temp;
+	int i,n,j,no,c;
+	printf("ENTER NO OF NODES:");
+	scanf("%d",&n);
+	printf("Enter node no 1:");
+	start=(node*)malloc(size of (node));
+	scanf("%d",&start->data);
+	temp=start;
+	for(i=1;i<n;i++)
+	{
+		ptr=(node*)malloc(size of (node));
+		printf("Enter node no:",i+1);
+		scanf("%d"&ptr->data);
+		temp=next->ptr;
+		temp=ptr;
+		temp+temp->next};
+		}
+		temp->next=null;
+		do
+		{
+			printf("Enter your choice");
+			printf("\n1-INSERTION\N2-DELETION\N3-TRANSVERSE\N4-EXIT");
+			scanf("%d"&j);
+			switch(j)
+			{
+				case 1:
+				insbeg(&start,no);
+				break;
+				case 2:
+				insend(&start,no);
+				break;
+				case 3:
+				insafter(start,no);
+				break;
+				default:
+				printf("Invalid Choice");
+				break;
+			}
+			break;
+			case 2:
+			printf("\nENTER THE NO. TO BE DELETED:");
+			scanf("");                                                                                            
+				
+			}
